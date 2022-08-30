94386cc452b5a5e38c20bbef36ca0d2a6412e7d8
SaltmineMP4 <erhen@protonmail.ch>
2022-08-30 16:02:57 +0200

    initinal commit

---
+++ contactFormSwitchSize.js
@@ -0,0 +1,29 @@
+function testGrootFunc() {
+  var GrootHide = document.getElementById("kleinBox1");
+  GrootHide.style.display = "none";
+    var Groot = document.getElementById("grootBox1");
+    if (Groot.style.display === "none") {
+      Groot.style.display = "block";
+    } else {
+      Groot.style.display = "none";
+    }
+}
+function testKleinFunc() {
+  var KleinHideG = document.getElementById("grootBox1");
+  KleinHideG.style.display = "none";
+  var KleinHideI = document.get
+    var klein = document.getElementById("kleinBox1");
+    if (klein.style.display === "none") {
+      klein.style.display = "block";
+    } else {
+      klein.style.display = "none";
+    }
+}
+function testInfoFunc() {
+    var info = document.getElementById("InfoBox1");
+    if (info.style.display === "block") {
+      info.style.display = "none";
+    } else {
+      info.style.display = "block";
+    }
+}
