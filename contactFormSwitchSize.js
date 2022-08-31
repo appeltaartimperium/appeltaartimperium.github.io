35ba5aa44704a0a458134805af0dd592832535bc
SaltmineMP4 <erhen@protonmail.ch>
2022-08-31 14:50:03 +0200

    2 box test

--- contactFormSwitchSize.js
+++ contactFormSwitchSize.js
@@ -1,29 +1,56 @@
-function testGrootFunc() {
-  var GrootHide = document.getElementById("kleinBox1");
-  GrootHide.style.display = "none";
-    var Groot = document.getElementById("grootBox1");
-    if (Groot.style.display === "none") {
-      Groot.style.display = "block";
+function testGrootFunc1() {
+  var GrootHide1 = document.getElementById("kleinBox1");
+  GrootHide1.style.display = "none";
+    var Groot1 = document.getElementById("grootBox1");
+    if (Groot1.style.display === "none") {
+      Groot1.style.display = "block";
     } else {
-      Groot.style.display = "none";
+      Groot1.style.display = "none";
     }
 }
-function testKleinFunc() {
-  var KleinHideG = document.getElementById("grootBox1");
-  KleinHideG.style.display = "none";
-  var KleinHideI = document.get
-    var klein = document.getElementById("kleinBox1");
-    if (klein.style.display === "none") {
-      klein.style.display = "block";
+function testKleinFunc1() {
+  var KleinHideG1 = document.getElementById("grootBox1");
+  KleinHideG1.style.display = "none";
+    var klein1 = document.getElementById("kleinBox1");
+    if (klein1.style.display === "none") {
+      klein1.style.display = "block";
     } else {
-      klein.style.display = "none";
+      klein1.style.display = "none";
     }
 }
-function testInfoFunc() {
-    var info = document.getElementById("InfoBox1");
-    if (info.style.display === "block") {
-      info.style.display = "none";
+function testInfoFunc1() {
+    var info1 = document.getElementById("InfoBox1");
+    if (info1.style.display === "block") {
+      info1.style.display = "none";
     } else {
-      info.style.display = "block";
+      info1.style.display = "block";
+    }
+}
+function testGrootFunc2() {
+  var GrootHide2 = document.getElementById("kleinBox2");
+  GrootHide2.style.display = "none";
+    var Groot2 = document.getElementById("grootBox2");
+    if (Groot2.style.display === "none") {
+      Groot2.style.display = "block";
+    } else {
+      Groot2.style.display = "none";
+    }
+}
+function testKleinFunc2() {
+  var KleinHideG2 = document.getElementById("grootBox2");
+  KleinHideG2.style.display = "none";
+    var klein2 = document.getElementById("kleinBox2");
+    if (klein2.style.display === "none") {
+      klein2.style.display = "block";
+    } else {
+      klein2.style.display = "none";
+    }
+}
+function testInfoFunc2() {
+    var info2 = document.getElementById("InfoBox2");
+    if (info2.style.display === "block") {
+      info2.style.display = "none";
+    } else {
+      info2.style.display = "block";
     }
 }
