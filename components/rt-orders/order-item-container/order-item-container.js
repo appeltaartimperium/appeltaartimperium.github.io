38d8c888a8c7b074f101f444df7083140f925610
Danny Engelman <danny@engelman.nl>
2022-10-12 20:48:54 +0200

    creating order-items-containers dynamically from orders.json file

--- components/rt-orders/order-item-container/order-item-container.js
+++ components/rt-orders/order-item-container/order-item-container.js
@@ -7,12 +7,15 @@ let uri = import.meta.url;
 customElements.define(
   componentName,
   class extends customElements.get(BaseClass) {
+    // ---------------------------------------------------------------- observedAttributes
     static get observedAttributes() {
       return ["piesize"];
     }
+    // ---------------------------------------------------------------- constructor
     constructor() {
       super().attachShadow({ mode: "open" }).append(this.getTemplate());
     }
+    // ---------------------------------------------------------------- connectedCallback
     connectedCallback() {
       setTimeout(() => {
         this.setPieSize();
@@ -22,15 +25,18 @@ customElements.define(
         this.setAttribute("piesize", evt.detail);
       });
     }
+    // ---------------------------------------------------------------- setPieSize
     setPieSize(size = this.getAttribute("piesize")) {
       // size = large || small
       let style = this.shadowRoot.querySelector("#piesize");
       style.innerHTML = `slot[name="${size}pie"] {display:initial!important}`;
     }
+    // ---------------------------------------------------------------- attributeChangedCallback
     attributeChangedCallback(name, oldValue, newValue) {
       if (name === "piesize") {
         this.setPieSize(newValue);
       }
     }
+    // ----------------------------------------------------------------
   }
 );
